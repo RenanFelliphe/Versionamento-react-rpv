369e56bfb0635196ed3bfb6cb9155fc190197d42
danielsilvabm <daniel.silvabmartins@gmail.com>
2026-04-14 21:30:55 -0300

    feat: cards de rooteamento da página home para a página de cada exercício

--- src/pages/exercicio-3/index.tsx
+++ src/pages/exercicio-3/index.tsx
@@ -1,10 +1,10 @@
-export default function Ex01() {
+export default function Ex03() {
 
     return(
         <>
             <h1 className="text-[2rem] fixed top-0 left-[50%] -translate-x-[50%] bg-[#0a0a0a] px-15 w-fit">
                 <span>Exercícios</span>
-                <span className="text-sky-500 font-bold"> 1</span>
+                <span className="text-sky-500 font-bold"> 3</span>
             </h1>        
         </>
     )
--- src/pages/exercicio-7/index.tsx
+++ src/pages/exercicio-7/index.tsx
@@ -1,11 +1,11 @@
-export default function Ex01() {
+export default function Ex07() {
 
     return(
         <>
             <h1 className="text-[2rem] fixed top-0 left-[50%] -translate-x-[50%] bg-[#0a0a0a] px-15 w-fit">
                 <span>Exercícios</span>
-                <span className="text-sky-500 font-bold"> 1</span>
-            </h1>        
+                <span className="text-sky-500 font-bold"> 7</span>
+            </h1>
         </>
     )
 }
