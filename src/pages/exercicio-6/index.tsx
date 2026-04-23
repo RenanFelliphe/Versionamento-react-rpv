f83ea559bbe6b8f9cf16b0c40da4c1f9c0b82fdb
danielsilvabm <daniel.silvabmartins@gmail.com>
2026-04-22 21:08:42 -0300

    Exercício 6 completo

--- src/pages/exercicio-6/index.tsx
+++ src/pages/exercicio-6/index.tsx
@@ -11,6 +11,7 @@ export default function Ex06() {
 
     const [users, setUser] = useState<any[]>([])
     const [numUser, setNumUser] = useState<number>(2)
+    const [currentUserName, setCurrentUserName] = useState<string>("")
 
     useEffect(() => {
         async function renderizaUsuarios() {
@@ -21,33 +22,30 @@ export default function Ex06() {
         }
 
         renderizaUsuarios()
-    })
+    }, [])
 
 
     return(
         <>
             <div className="flex flex-col items-center justify-center h-screen w-full">
-                <h1 className="text-[2rem] fixed top-0 left-[50%] -translate-x-[50%] bg-[#0a0a0a] px-15 w-fit">
+                <h1 className="text-[2rem] bg-[#0a0a0a] px-15 w-fit">
                     <span>Exercícios</span>
                     <span className="text-sky-500 font-bold"> 6</span>
                 </h1>
 
-                <div className="relative border border-slate-400 rounded-xl p-10 h-fit w-200 gap-5 flex flex-col items-center justify-center">
-                    <h2 className="text-[2rem] absolute -top-6 left-[50%] -translate-x-[50%] bg-[#0a0a0a] px-15 w-fit whitespace-nowrap">Catálogo de Animes</h2>
-
-                    <input type="search" className="border border-slate-400 rounded-xl py-1 px-5 w-full py-2 px-5" placeholder="Pesquisar anime..."/>
-
+                <div className="mt-10 relative border border-slate-400 rounded-xl p-10 h-fit w-200 gap-5 flex flex-col items-center justify-center">
+                    <h2 className="text-[2rem] absolute -top-6 left-[50%] -translate-x-[50%] bg-[#0a0a0a] px-15 w-fit whitespace-nowrap">Busca e Filtro de Usuários</h2>
+                    <input type="search" className="border border-slate-400 rounded-xl py-1 px-5 w-full py-2 px-5" placeholder="Pesquisar anime..." onChange={(e) => {setCurrentUserName(e.target.value)}}/>
                     <div className="grid grid-cols-6 gap-5 pl-2">
                         {
-                            users.length ? (users.map((user) => {
-                                return (
-                                    <div key={user.id} className="col-span-2 border border-slate-400 rounded-xl py-2 px-10 cursor-pointer hover:shadow-md shadow-gray-500">
-                                        <p>Usuário: {user.id} - {user.title}</p>
-                                    </div>
-                                )
-                                })) : (
-                                    <p>Nenhum usuário registrado</p>
-                                )
+                            users.filter(user => 
+                                user.title.toLowerCase().includes(currentUserName.toLowerCase())
+                            ).map(user => (
+                                <div key={user.id} className="col-span-2 border border-slate-400 rounded-xl py-2 px-10 cursor-pointer hover:shadow-md shadow-gray-500">
+                                    <p className="text-center">Usuário: {user.id}</p>
+                                    <p className="text-justify">Título: {user.title}</p>
+                                </div>
+                            ))
                         }
                     </div>  
                 </div>
